1a64ff39c185841528d6b398fd0744319eae5632
kourosh <the1kouroshkhalili@gmail.com>
2024-09-21 08:30:21 +0330

    just for testing my branch

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
  */
 
 Route::post('/todos', [TodoController::class, 'store']);
+Route::get('/todos', [TodoController::class, 'index']);
